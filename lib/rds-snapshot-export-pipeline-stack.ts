53f8a8b3c8f83dc130f3e836c6bc28fed88e2dab
Nguyễn Bảo Khôi <nbkhoi@tma.com.vn>
2023-02-02 15:47:18 +0700

    Feat: auto exporting snapshot to s3

--- lib/rds-snapshot-export-pipeline-stack.ts
+++ lib/rds-snapshot-export-pipeline-stack.ts
@@ -13,8 +13,27 @@ import { Code, FunctionUrl, FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { Action } from 'aws-cdk-lib/aws-codepipeline';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { CfnEventSubscription } from 'aws-cdk-lib/aws-rds';
+import { SnsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 const accountRootPrincipal = new AccountRootPrincipal();
+
+export enum RdsEventId {
+	/**
+	 * Event IDs for which the Lambda supports starting a snapshot export task.
+	 *
+	 * See:
+	 *   https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/USER_Events.Messages.html#USER_Events.Messages.cluster-snapshot
+	 *   https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Events.Messages.html#USER_Events.Messages.snapshot
+	 */
+	// For automated snapshots of Aurora RDS clusters
+	DB_AUTOMATED_AURORA_SNAPSHOT_CREATED = "RDS-EVENT-0169",
+
+	// For automated snapshots of non-Aurora RDS clusters
+	DB_AUTOMATED_SNAPSHOT_CREATED = "RDS-EVENT-0091"
+}
+
 export class RdsSnapshotExportPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -59,18 +78,37 @@ export class RdsSnapshotExportPipelineStack extends cdk.Stack {
       },
       resources: ['*']
     }));
+
+		const snapshotEventTopic = new Topic(this, "SnapshotEventTopic", {
+			displayName: "rds-snapshot-creation"
+		});
+
+		new CfnEventSubscription(this, 'RdsSnapshotEventNotification', {
+			snsTopicArn: snapshotEventTopic.topicArn,
+			enabled: true,
+			eventCategories: ['creation'],
+			sourceType:'db-snapshot',
+		});
+
     const rdsSnapshotExporterLambdaFunction = new lambda.Function(this, "RdsSnapshotExporterLambdaFunction", {
       functionName: `${config.get('environmentname')}RdsSnapshotExporterLambdaFunction`,
       runtime: Runtime.PYTHON_3_8,
       handler: "main.handler",
       code: Code.fromAsset(path.join(__dirname, "/../assets/exporter/")),
       environment: {
+				RDS_EVENT_ID: RdsEventId.DB_AUTOMATED_AURORA_SNAPSHOT_CREATED,
+				DB_NAME: dbName,
         LOG_LEVEL: "INFO",
+				SNAPSHOT_BUCKET_NAME: backupS3BucketName,
         SNAPSHOT_TASK_ROLE: rdsSnapshotExportTaskRole.roleArn,
-        SNAPSHOT_TASK_KEY: rdsSnapshotExportEncryptionKey.keyArn
+        SNAPSHOT_TASK_KEY: rdsSnapshotExportEncryptionKey.keyArn,
+				DB_SNAPSHOT_TYPE: "snapshot",
       },
       role: lambdaExecutionRole,
-      timeout: cdk.Duration.seconds(30)
+      timeout: cdk.Duration.seconds(30),
+			events: [
+				new SnsEventSource(snapshotEventTopic)
+			]
     });
     rdsSnapshotExporterLambdaFunction.applyRemovalPolicy(config.get('defaultremovalpolicy'));
     new FunctionUrl(this, "rdsSnapshotExporterLambdaFunctionUrl", {
