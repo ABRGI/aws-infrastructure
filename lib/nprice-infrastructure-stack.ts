578ca46f17fffeebccd63bba91fe098ece81f8ce
Nguyen Minh Duc <nmduc1@tma.com.vn>
2024-04-09 16:38:29 +0700

    feat: SAAS-1537 define attribute to enable/disable nprice core instance

--- lib/nprice-infrastructure-stack.ts
+++ lib/nprice-infrastructure-stack.ts
@@ -114,133 +114,135 @@ export class NpriceInfrastructureStack extends cdk.Stack {
         const multipartUserData = new ec2.MultipartUserData();
         multipartUserData.addPart(ec2.MultipartBody.fromUserData(setupCommands));
 
-        const npriceApiInstance = new Instance(this, `${config.get('environmentname')}NpriceApiInstance`, {
-            instanceType: InstanceType.of(
-                InstanceClass.T2,
-                InstanceSize.MICRO,
-            ),
-            machineImage: ec2.MachineImage.latestAmazonLinux({
-                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-            }),
-            vpc: props.vpc as Vpc,
-            securityGroup: npriceApiSG,
-            instanceName: `${config.get('environmentname')}-nprice-api`,
-            role: npriceRole,
-            userData: multipartUserData
-        });
-        npriceApiInstance.applyRemovalPolicy(config.get('defaultremovalpolicy'));
+        if (config.get('npriceinfrastructurestack.isactivatednpricecore')) {
+            const npriceApiInstance = new Instance(this, `${config.get('environmentname')}NpriceApiInstance`, {
+                instanceType: InstanceType.of(
+                    InstanceClass.T2,
+                    InstanceSize.MICRO,
+                ),
+                machineImage: ec2.MachineImage.latestAmazonLinux({
+                    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+                }),
+                vpc: props.vpc as Vpc,
+                securityGroup: npriceApiSG,
+                instanceName: `${config.get('environmentname')}-nprice-api`,
+                role: npriceRole,
+                userData: multipartUserData
+            });
+            npriceApiInstance.applyRemovalPolicy(config.get('defaultremovalpolicy'));
+    
+            // Configurations for nprice core
+            const npriceCoreSG = new ec2.SecurityGroup(this, `${config.get('environmentname')}NpriceCoreSecurityGroup`, {
+                vpc: props.vpc,
+                allowAllOutbound: true,
+                description: 'Security group for nprice core',
+                securityGroupName: `${config.get('environmentname')}-nprice-core-sg`
+            });
+            npriceCoreSG.connections.allowFrom(bastionSG, ec2.Port.tcp(22));
+            npriceCoreSG.applyRemovalPolicy(config.get('defaultremovalpolicy'));
+            cdk.Aspects.of(npriceCoreSG).add(
+                new cdk.Tag('Name', `${config.get('environmentname')}-nprice-core-sg`)
+            );
+            cdk.Aspects.of(npriceCoreSG).add(
+                new cdk.Tag('nelson:client', 'saas')
+            );
+            cdk.Aspects.of(npriceCoreSG).add(
+                new cdk.Tag('nelson:role', 'service')
+            );
+            cdk.Aspects.of(npriceCoreSG).add(
+                new cdk.Tag('nelson:environment', config.get('environmentname'))
+            );
 
-        // Configurations for nprice core
-        const npriceCoreSG = new ec2.SecurityGroup(this, `${config.get('environmentname')}NpriceCoreSecurityGroup`, {
-            vpc: props.vpc,
-            allowAllOutbound: true,
-            description: 'Security group for nprice core',
-            securityGroupName: `${config.get('environmentname')}-nprice-core-sg`
-        });
-        npriceCoreSG.connections.allowFrom(bastionSG, ec2.Port.tcp(22));
-        npriceCoreSG.applyRemovalPolicy(config.get('defaultremovalpolicy'));
-        cdk.Aspects.of(npriceCoreSG).add(
-            new cdk.Tag('Name', `${config.get('environmentname')}-nprice-core-sg`)
-        );
-        cdk.Aspects.of(npriceCoreSG).add(
-            new cdk.Tag('nelson:client', 'saas')
-        );
-        cdk.Aspects.of(npriceCoreSG).add(
-            new cdk.Tag('nelson:role', 'service')
-        );
-        cdk.Aspects.of(npriceCoreSG).add(
-            new cdk.Tag('nelson:environment', config.get('environmentname'))
-        );
-
-        const npriceCoreInstance = new Instance(this, `${config.get('environmentname')}NpriceCoreInstance`, {
-            instanceType: InstanceType.of(
-                InstanceClass.M4,
-                InstanceSize.XLARGE
-            ),
-            machineImage: ec2.MachineImage.latestAmazonLinux({
-                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-            }),
-            vpc: props.vpc as Vpc,
-            securityGroup: npriceCoreSG,
-            instanceName: `${config.get('environmentname')}-nprice-core`,
-            role: npriceRole,
-            userData: multipartUserData
-        });
-        npriceCoreInstance.applyRemovalPolicy(config.get('defaultremovalpolicy'));
-
-        const npriceCoreStarerScript = new lambda.Function(this, 'NpriceCoreStarerScript', {
-            runtime: lambda.Runtime.PYTHON_3_7,
-            functionName: `${config.get('environmentname')}-nprice_core_starter_script`,
-            architecture: lambda.Architecture.X86_64,
-            handler: 'lambda_function.lambda_handler',
-            code: lambda.Code.fromAsset(path.join(__dirname, '/../assets/nprice-core')),
-            timeout: cdk.Duration.seconds(15),
-            description: 'This fucntion checks that the nPrice core is stopped and starts it, otherwise it will sen a notification.',
-            environment: {
-                NPRICE_CORE_INSTANCE_ID: npriceCoreInstance.instanceId,
-                NPRICE_CORE_INSTANCE_NAME: config.get('environmentname'),
-                SNS_ARN: config.get('npriceinfrastructurestack.snsarn'),
-                REGION: config.get('npriceinfrastructurestack.region')
-            },
-        });
-        npriceCoreStarerScript.applyRemovalPolicy(config.get('defaultremovalpolicy'));
-        cdk.Aspects.of(npriceCoreStarerScript).add(
-            new cdk.Tag('Name', `${config.get('environmentname')}-nprice-starter-script`)
-        );
-        cdk.Aspects.of(npriceCoreStarerScript).add(
-            new cdk.Tag('nelson:client', 'saas')
-        );
-        cdk.Aspects.of(npriceCoreStarerScript).add(
-            new cdk.Tag('nelson:role', 'service')
-        );
-        cdk.Aspects.of(npriceCoreStarerScript).add(
-            new cdk.Tag('nelson:environment', config.get('environmentname'))
-        );
-
-        npriceCoreStarerScript.addToRolePolicy(PolicyStatement.fromJson(
-            {
-                'Sid': 'EC2StartDescribe',
-                'Effect': 'Allow',
-                'Action': 'ec2:DescribeInstances',
-                'Resource': '*'
-            },
-        ));
-        npriceCoreStarerScript.addToRolePolicy(PolicyStatement.fromJson(
-            {
-                'Sid': 'SNSPublish',
-                'Effect': 'Allow',
-                'Action': [
-                    'sns:Publish',
-                    'ec2:StartInstances'
-                ],
-                'Resource': [
-                    config.get('npriceinfrastructurestack.snsarn'),
-                    `arn:aws:ec2:${this.region}:${this.account}:instance/${npriceCoreInstance.instanceId}`
+            const npriceCoreInstance = new Instance(this, `${config.get('environmentname')}NpriceCoreInstance`, {
+                instanceType: InstanceType.of(
+                    InstanceClass.M4,
+                    InstanceSize.XLARGE
+                ),
+                machineImage: ec2.MachineImage.latestAmazonLinux({
+                    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+                }),
+                vpc: props.vpc as Vpc,
+                securityGroup: npriceCoreSG,
+                instanceName: `${config.get('environmentname')}-nprice-core`,
+                role: npriceRole,
+                userData: multipartUserData
+            });
+            npriceCoreInstance.applyRemovalPolicy(config.get('defaultremovalpolicy'));
+    
+            const npriceCoreStarerScript = new lambda.Function(this, 'NpriceCoreStarerScript', {
+                runtime: lambda.Runtime.PYTHON_3_7,
+                functionName: `${config.get('environmentname')}-nprice_core_starter_script`,
+                architecture: lambda.Architecture.X86_64,
+                handler: 'lambda_function.lambda_handler',
+                code: lambda.Code.fromAsset(path.join(__dirname, '/../assets/nprice-core')),
+                timeout: cdk.Duration.seconds(15),
+                description: 'This fucntion checks that the nPrice core is stopped and starts it, otherwise it will sen a notification.',
+                environment: {
+                    NPRICE_CORE_INSTANCE_ID: npriceCoreInstance.instanceId,
+                    NPRICE_CORE_INSTANCE_NAME: config.get('environmentname'),
+                    SNS_ARN: config.get('npriceinfrastructurestack.snsarn'),
+                    REGION: config.get('npriceinfrastructurestack.region')
+                },
+            });
+            npriceCoreStarerScript.applyRemovalPolicy(config.get('defaultremovalpolicy'));
+            cdk.Aspects.of(npriceCoreStarerScript).add(
+                new cdk.Tag('Name', `${config.get('environmentname')}-nprice-starter-script`)
+            );
+            cdk.Aspects.of(npriceCoreStarerScript).add(
+                new cdk.Tag('nelson:client', 'saas')
+            );
+            cdk.Aspects.of(npriceCoreStarerScript).add(
+                new cdk.Tag('nelson:role', 'service')
+            );
+            cdk.Aspects.of(npriceCoreStarerScript).add(
+                new cdk.Tag('nelson:environment', config.get('environmentname'))
+            );
+    
+            npriceCoreStarerScript.addToRolePolicy(PolicyStatement.fromJson(
+                {
+                    'Sid': 'EC2StartDescribe',
+                    'Effect': 'Allow',
+                    'Action': 'ec2:DescribeInstances',
+                    'Resource': '*'
+                },
+            ));
+            npriceCoreStarerScript.addToRolePolicy(PolicyStatement.fromJson(
+                {
+                    'Sid': 'SNSPublish',
+                    'Effect': 'Allow',
+                    'Action': [
+                        'sns:Publish',
+                        'ec2:StartInstances'
+                    ],
+                    'Resource': [
+                        config.get('npriceinfrastructurestack.snsarn'),
+                        `arn:aws:ec2:${this.region}:${this.account}:instance/${npriceCoreInstance.instanceId}`
+                    ]
+                },
+            ));
+    
+            const nPriceCoreStartEventTrigger = new Rule(this, 'NpriceCoreStarterRule', {
+                ruleName: `${config.get('environmentname')}-nPriceCoreStarter`,
+                description: `Event to trigger the nprice starter lambda function for ${config.get('environmentname')} environment`,
+                enabled: true,
+                schedule: Schedule.cron(config.get('npriceinfrastructurestack.corebootcron')),
+                targets: [
+                    new cdk.aws_events_targets.LambdaFunction(npriceCoreStarerScript)
                 ]
-            },
-        ));
-
-        const nPriceCoreStartEventTrigger = new Rule(this, 'NpriceCoreStarterRule', {
-            ruleName: `${config.get('environmentname')}-nPriceCoreStarter`,
-            description: `Event to trigger the nprice starter lambda function for ${config.get('environmentname')} environment`,
-            enabled: true,
-            schedule: Schedule.cron(config.get('npriceinfrastructurestack.corebootcron')),
-            targets: [
-                new cdk.aws_events_targets.LambdaFunction(npriceCoreStarerScript)
-            ]
-        });
-        nPriceCoreStartEventTrigger.applyRemovalPolicy(config.get('defaultremovalpolicy'));
-        cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
-            new cdk.Tag('Name', `${config.get('environmentname')}-nprice-starter-script`)
-        );
-        cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
-            new cdk.Tag('nelson:client', 'saas')
-        );
-        cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
-            new cdk.Tag('nelson:role', 'service')
-        );
-        cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
-            new cdk.Tag('nelson:environment', config.get('environmentname'))
-        );
+            });
+            nPriceCoreStartEventTrigger.applyRemovalPolicy(config.get('defaultremovalpolicy'));
+            cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
+                new cdk.Tag('Name', `${config.get('environmentname')}-nprice-starter-script`)
+            );
+            cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
+                new cdk.Tag('nelson:client', 'saas')
+            );
+            cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
+                new cdk.Tag('nelson:role', 'service')
+            );
+            cdk.Aspects.of(nPriceCoreStartEventTrigger).add(
+                new cdk.Tag('nelson:environment', config.get('environmentname'))
+            );
+        }
     }
 }
